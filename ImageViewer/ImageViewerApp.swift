e29b80634afeb500efc361171dd63db984502ded
pratyushsanadhya <72932926+pratyushsanadhya@users.noreply.github.com>
2023-02-19 10:14:55 +0530

    Initial commit

---
+++ ImageViewer/ImageViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageViewerApp.swift
+//  ImageViewer
+//
+//  Created by Pratyush Sanadhya on 18/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ImageViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
